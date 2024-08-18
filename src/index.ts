49f0951a5db87b24de34af096247c96e0831f71e
CJ Pais <cj@cjpais.com>
2024-08-18 14:35:48 -0700

    add better support for link copy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { getFileHandler, parseGetFileRequest } from "./handlers/public/getFile";
 import { storeHandler } from "./handlers/store";
-import { EXTERNAL_SERVICE_PORT, INTERNAL_SERVICE_PORT } from "./lib/env";
+import {
+  BASE_EXTERNAL_URL,
+  EXTERNAL_SERVICE_PORT,
+  INTERNAL_SERVICE_PORT,
+} from "./lib/env";
 
 const startFrontendServer = () => {
   console.log(`Starting frontend server on port ${EXTERNAL_SERVICE_PORT}`);
@@ -26,8 +30,16 @@ const startBackendServer = () => {
     async fetch(req) {
       const url = new URL(req.url);
       if (url.pathname === "/store") return await storeHandler(req);
-      if (url.pathname === "/")
-        return new Response(Bun.file("./src/ui/backend/index.html"));
+      if (url.pathname === "/") {
+        const template = await Bun.file("./src/ui/backend/index.html").text();
+        const html = template.replace("{{BASE_URL}}", BASE_EXTERNAL_URL);
+
+        return new Response(html, {
+          headers: {
+            "Content-Type": "text/html",
+          },
+        });
+      }
       // TODO PATCH for updating metadata?
       // TODO this is a regex match on the path
       //   if (url.pathname === "/delete") return await deleteHandler(req);
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 const EnvSchema = z.object({
   INTERNAL_SERVICE_PORT: z.string().regex(/^\d+$/).transform(Number),
   EXTERNAL_SERVICE_PORT: z.string().regex(/^\d+$/).transform(Number),
+  EXTERNAL_SERVICE_DOMAIN: z.string(),
   STORE_DIR: z.string().default("./.store"),
 });
 
@@ -18,5 +19,19 @@ if (!envResult.success) {
   process.exit(1);
 }
 
-export const { INTERNAL_SERVICE_PORT, EXTERNAL_SERVICE_PORT, STORE_DIR } =
-  envResult.data;
+export const {
+  INTERNAL_SERVICE_PORT,
+  EXTERNAL_SERVICE_PORT,
+  STORE_DIR,
+  EXTERNAL_SERVICE_DOMAIN,
+} = envResult.data;
+
+export const isDev = process.env.NODE_ENV === "development";
+
+export const BASE_INTERNAL_URL = isDev
+  ? `http://localhost:${INTERNAL_SERVICE_PORT}`
+  : `https://http://cjs-mac-mini-2.tail73f30.ts.net:${INTERNAL_SERVICE_PORT}`;
+
+export const BASE_EXTERNAL_URL = isDev
+  ? `http://localhost:${EXTERNAL_SERVICE_PORT}`
+  : EXTERNAL_SERVICE_DOMAIN;
