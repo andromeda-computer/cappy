77ec2036056e3e70de4e4f2568db482c0f53369d
CJ Pais <cj@cjpais.com>
2024-08-27 11:10:17 -0700

    decode uri encoded strings on the server

--- packages/store/src/servers/file.ts
+++ packages/store/src/servers/file.ts
@@ -11,39 +11,40 @@ app.get("/", async (c) => {
   return c.html("cappy");
 });
 
-app.get(
-  "/:username{[a-zA-Z0-9_-]+}/:filename{[a-fA-F0-9]{64}|[a-zA-Z0-9._-]+}",
-  async (c) => {
-    const { username, filename } = c.req.param();
-    const isHash = /^[a-fA-F0-9]{64}$/.test(filename);
-    const rangeHeader = c.req.header("Range");
-    console.log({ rangeHeader });
+app.get("/:username{[a-zA-Z0-9_-]+}/:filename", async (c) => {
+  let { username, filename } = c.req.param();
+  const isHash = /^[a-fA-F0-9]{64}$/.test(filename);
+  const rangeHeader = c.req.header("Range");
+  console.log({ rangeHeader });
 
-    const dbFile = getFile({ username, filename, isHash });
+  if (!isHash) username = decodeURIComponent(username);
 
-    // if file not found
-    if (!dbFile) return c.text("File not found", { status: 404 });
+  console.log({ username, filename, isHash });
 
-    // if file is unlisted
-    if (!isHash && dbFile.visibility === "unlisted") {
-      console.log(
-        `warning: someone tried to access the unlisted file: ${username}/${filename}`
-      );
-      return c.text("File not found", { status: 404 });
-    }
+  const dbFile = getFile({ username, filename, isHash });
 
-    const filepath = `${STORE_DIR}/${dbFile.username}/${dbFile.hash}/data.${dbFile.extension}`;
-    const file = Bun.file(filepath);
+  // if file not found
+  if (!dbFile) return c.text("File not found", { status: 404 });
 
-    if (await file.exists()) {
-      console.log(file.type);
-      // @ts-ignore
-      return c.body(file);
-    } else {
-      return c.text("File not found", { status: 404 });
-    }
+  // if file is unlisted
+  if (!isHash && dbFile.visibility === "unlisted") {
+    console.log(
+      `warning: someone tried to access the unlisted file: ${username}/${filename}`
+    );
+    return c.text("File not found", { status: 404 });
   }
-);
+
+  const filepath = `${STORE_DIR}/${dbFile.username}/${dbFile.hash}/data.${dbFile.extension}`;
+  const file = Bun.file(filepath);
+
+  if (await file.exists()) {
+    console.log(file.type);
+    // @ts-ignore
+    return c.body(file);
+  } else {
+    return c.text("File not found", { status: 404 });
+  }
+});
 
 console.log(`Starting file server on port ${EXTERNAL_SERVICE_PORT}`);
 export default {
